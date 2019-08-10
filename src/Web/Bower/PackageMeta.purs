dab96f66ac3867bc816c789f4b7486e637e87f62
klntsky <klntsky@gmail.com>
2019-08-10 13:20:29 +0300

    added: BowerJson

--- src/Web/Bower/PackageMeta.purs
+++ src/Web/Bower/PackageMeta.purs
@@ -1,9 +1,21 @@
 -- | Decoders for various types representing `bower.json` contents.
 -- |
--- | `PackageMeta` represents a valid `bower.json` file.
+-- | The main type is `PackageMeta`.
+-- |
+-- | `BowerJson` represents a valid `bower.json` file (literally).
 -- |
 -- | The spec: https://github.com/bower/spec/blob/master/json.md
-module Web.Bower.PackageMeta where
+module Web.Bower.PackageMeta
+       ( PackageMeta (..)
+       , Dependencies (..)
+       , Resolutions (..)
+       , ModuleType (..)
+       , Author (..)
+       , BowerJson (..)
+       , readPackageMeta
+       , writePackageMeta
+       )
+where
 
 import Prelude
 
@@ -12,13 +24,217 @@ import Data.Argonaut.Core (Json, caseJsonString, fromString, jsonEmptyObject)
 import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.:), (.:?))
 import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (:=?), (~>), (~>?))
 import Data.Either (Either(..))
-import Data.Foldable (foldr)
+import Data.Foldable (fold, foldr)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Newtype (class Newtype, unwrap)
 import Data.Tuple (Tuple(..))
 import Foreign.Object as Object
+
+-- | A human-friendly version of `BowerJson`.
+-- |
+-- | Decoding to and encoding from this type does not preserve fields with
+-- | default values.
+-- |
+-- | For example, encoding a `PackageMeta` with `license: []` and
+-- | `private: false` will result in a json object without these fields.
+-- |
+-- | Use `BowerJson` if these omissions are not desirable.
+newtype PackageMeta = PackageMeta
+  { name            :: String
+  , description     :: Maybe String
+  , main            :: Array String
+  , moduleType      :: Array ModuleType
+  , license         :: Array String
+  , ignore          :: Array String
+  , keywords        :: Array String
+  , authors         :: Array Author
+  , homepage        :: Maybe String
+  , repository      :: Maybe { url :: String
+                             , type :: String
+                             }
+  , dependencies    :: Dependencies
+  , devDependencies :: Dependencies
+  , resolutions     :: Resolutions
+  , private         :: Boolean
+  }
+
+derive instance eqPackageMeta :: Eq PackageMeta
+derive instance newtypePackageMeta :: Newtype PackageMeta _
+derive instance genericPackageMeta :: Generic PackageMeta _
+instance showPackageMeta :: Show PackageMeta where
+  show = genericShow
+
+instance decodeJsonPackageMeta :: DecodeJson PackageMeta where
+  decodeJson = map readPackageMeta <<< decodeJson
+
+instance encodeJsonPackageMeta :: EncodeJson PackageMeta where
+  encodeJson = writePackageMeta >>> encodeJson
+
+readPackageMeta :: BowerJson -> PackageMeta
+readPackageMeta (BowerJson { name
+                           , description
+                           , main
+                           , moduleType
+                           , license
+                           , ignore
+                           , keywords
+                           , resolutions
+                           , private
+                           , dependencies
+                           , devDependencies
+                           , repository
+                           , authors
+                           , homepage
+                           }) =
+  PackageMeta { name, description
+              , main: fold main
+              , moduleType: fold moduleType
+              , license: fold license
+              , ignore: fold ignore
+              , keywords: fold keywords
+              , resolutions: fold resolutions
+              , private: fromMaybe false private
+              , dependencies: dependencies
+              , devDependencies: devDependencies
+              , repository: repository
+              , authors: fold authors
+              , homepage: homepage
+              }
+
+writePackageMeta :: PackageMeta -> BowerJson
+writePackageMeta (PackageMeta { name
+                              , description
+                              , main
+                              , moduleType
+                              , license
+                              , ignore
+                              , keywords
+                              , resolutions
+                              , private
+                              , dependencies
+                              , devDependencies
+                              , repository
+                              , authors
+                              , homepage
+                              }) =
+  BowerJson { name, description
+            , main: unfold main
+            , moduleType: unfold moduleType
+            , license: unfold license
+            , ignore: unfold ignore
+            , keywords: unfold keywords
+            , resolutions: unfold resolutions
+            , private: if private == false then Nothing else Just true
+            , dependencies: dependencies
+            , devDependencies: devDependencies
+            , repository: repository
+            , authors: unfold authors
+            , homepage: homepage
+            }
+  where
+    unfold :: forall a. Eq a => Monoid a => a -> Maybe a
+    unfold x = if mempty == x then Nothing else Just x
+
+-- | A `bower.json` file represented as it is.
+-- | Decoding and encoding a `bower.json` to and from `BowerJson` does not change it
+-- | (up to JSON reformatting).
+-- |
+-- | `BowerJson` type should only be used when the above property is needed.
+-- |
+-- | For other use cases, it is more convenient to use `PackageMeta` instead.
+newtype BowerJson = BowerJson
+  { name            :: String
+  , description     :: Maybe String
+  , main            :: Maybe (Array String)
+  , moduleType      :: Maybe (Array ModuleType)
+  , license         :: Maybe (Array String)
+  , ignore          :: Maybe (Array String)
+  , keywords        :: Maybe (Array String)
+  , authors         :: Maybe (Array Author)
+  , homepage        :: Maybe String
+  , repository      :: Maybe { url :: String
+                             , type :: String
+                             }
+  , dependencies    :: Dependencies
+  , devDependencies :: Dependencies
+  , resolutions     :: Maybe Resolutions
+  , private         :: Maybe Boolean
+  }
+
+derive instance newtypeBowerJson :: Newtype BowerJson _
+derive instance eqBowerJson :: Eq BowerJson
+derive instance genericBowerJson :: Generic BowerJson _
+instance showBowerJson :: Show BowerJson where
+  show = genericShow
+
+instance decodeJsonBowerJson :: DecodeJson BowerJson where
+  decodeJson json = do
+
+    x               <- decodeJson json
+    name            <-               x .:  "name"
+    description     <-               x .:? "description"
+    main            <- maybeMany =<< x .:? "main"
+    moduleType      <- maybeMany =<< x .:? "moduleType"
+    license         <- maybeMany =<< x .:? "license"
+    ignore          <-               x .:? "ignore"
+    keywords        <-               x .:? "keywords"
+    dependencies    <-               x .:  "dependencies"
+    devDependencies <-               x .:  "devDependencies"
+    resolutions     <-               x .:? "resolutions"
+    private         <-               x .:? "private"
+    repository      <-               x .:? "repository"
+    authors         <-               x .:? "authors"
+    homepage        <-               x .:? "homepage"
+
+    pure $ BowerJson { name
+                     , description
+                     , main
+                     , moduleType
+                     , license
+                     , ignore
+                     , keywords
+                     , resolutions
+                     , private
+                     , dependencies
+                     , devDependencies
+                     , repository
+                     , authors
+                     , homepage
+                     }
+
+
+instance encodeJsonBowerJson :: EncodeJson BowerJson where
+  encodeJson (BowerJson { name
+                        , description
+                        , main
+                        , moduleType
+                        , license
+                        , ignore
+                        , keywords
+                        , resolutions
+                        , private
+                        , dependencies
+                        , devDependencies
+                        , repository
+                        , authors
+                        , homepage
+                        }) =
+    "name" := name ~>
+    "description" :=? description ~>?
+    "main" :=? main ~>?
+    "moduleType" :=? moduleType ~>?
+    "license" :=? license ~>?
+    "ignore" :=? ignore ~>?
+    "keywords" :=? keywords ~>?
+    "resolutions" :=? resolutions ~>?
+    "private" :=? private ~>?
+    "dependencies" := dependencies ~>
+    "devDependencies" := devDependencies ~>
+    "repository" :=? repository ~>?
+    "authors" :=? authors ~>?
+    "homepage" :=? homepage
 
 newtype Dependencies =
   Dependencies (Array { packageName :: String
@@ -28,6 +244,8 @@ newtype Dependencies =
 derive instance newtypeDependencies :: Newtype Dependencies _
 derive instance eqDependencies :: Eq Dependencies
 derive instance genericDependencies :: Generic Dependencies _
+derive newtype instance semigroupDependencies :: Semigroup Dependencies
+derive newtype instance monoidDependencies :: Monoid Dependencies
 instance showDependencies :: Show Dependencies where
   show = genericShow
 
@@ -53,6 +271,8 @@ newtype Resolutions =
 derive instance newtypeResolution :: Newtype Resolutions _
 derive instance eqResolutions :: Eq Resolutions
 derive instance genericResolutions :: Generic Resolutions _
+derive newtype instance semigroupResolutions :: Semigroup Resolutions
+derive newtype instance monoidResolutions :: Monoid Resolutions
 instance showResolutions :: Show Resolutions where
   show = genericShow
 
@@ -147,98 +367,6 @@ instance encodeJsonAuthor :: EncodeJson Author where
   encodeJson (Author { name, email, homepage }) =
     "name" :=? name ~>?
     "email" :=? email ~>?
-    "homepage" :=? homepage
-
-newtype PackageMeta = PackageMeta
-  { name            :: String
-  , description     :: Maybe String
-  , main            :: Maybe (Array String)
-  , moduleType      :: Maybe (Array ModuleType)
-  , license         :: Maybe (Array String)
-  , ignore          :: Maybe (Array String)
-  , keywords        :: Maybe (Array String)
-  , authors         :: Maybe (Array Author)
-  , homepage        :: Maybe String
-  , repository      :: Maybe { url :: String
-                             , type :: String
-                             }
-  , dependencies    :: Dependencies
-  , devDependencies :: Dependencies
-  , resolutions     :: Maybe Resolutions
-  , private         :: Maybe Boolean
-  }
-
-derive instance newtypePackageMeta :: Newtype PackageMeta _
-derive instance eqPackageMeta :: Eq PackageMeta
-derive instance genericPackageMeta :: Generic PackageMeta _
-instance showPackageMeta :: Show PackageMeta where
-  show = genericShow
-
-instance decodeJsonPackageMeta :: DecodeJson PackageMeta where
-  decodeJson json = do
-
-    x               <- decodeJson json
-    name            <-               x .:  "name"
-    description     <-               x .:? "description"
-    main            <- maybeMany =<< x .:? "main"
-    moduleType      <- maybeMany =<< x .:? "moduleType"
-    license         <- maybeMany =<< x .:? "license"
-    ignore          <-               x .:? "ignore"
-    keywords        <-               x .:? "keywords"
-    dependencies    <-               x .:  "dependencies"
-    devDependencies <-               x .:  "devDependencies"
-    resolutions     <-               x .:? "resolutions"
-    private         <-               x .:? "private"
-    repository      <-               x .:? "repository"
-    authors         <-               x .:? "authors"
-    homepage        <-               x .:? "homepage"
-
-    pure $ PackageMeta { name
-                       , description
-                       , main
-                       , moduleType
-                       , license
-                       , ignore
-                       , keywords
-                       , resolutions
-                       , private
-                       , dependencies
-                       , devDependencies
-                       , repository
-                       , authors
-                       , homepage
-                       }
-
-
-instance encodeJsonPackageMeta :: EncodeJson PackageMeta where
-  encodeJson (PackageMeta { name
-                          , description
-                          , main
-                          , moduleType
-                          , license
-                          , ignore
-                          , keywords
-                          , resolutions
-                          , private
-                          , dependencies
-                          , devDependencies
-                          , repository
-                          , authors
-                          , homepage
-                          }) =
-    "name" := name ~>
-    "description" :=? description ~>?
-    "main" :=? main ~>?
-    "moduleType" :=? moduleType ~>?
-    "license" :=? license ~>?
-    "ignore" :=? ignore ~>?
-    "keywords" :=? keywords ~>?
-    "resolutions" :=? resolutions ~>?
-    "private" :=? private ~>?
-    "dependencies" := dependencies ~>
-    "devDependencies" := devDependencies ~>
-    "repository" :=? repository ~>?
-    "authors" :=? authors ~>?
     "homepage" :=? homepage
 
 -- | Decode a *recommended* field that may contain a single value
--- test/Main.purs
+++ test/Main.purs
@@ -11,7 +11,7 @@ import Effect (Effect)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
-import Web.Bower.PackageMeta (Author(..), Dependencies(..), ModuleType(..), PackageMeta(..), Resolutions(..))
+import Web.Bower.PackageMeta (Author(..), Dependencies(..), ModuleType(..), BowerJson(..), Resolutions(..), PackageMeta(..))
 
 bower1 :: String
 bower1 = """
@@ -57,60 +57,112 @@ bower1 = """
 }
 """
 
-bower1Data :: PackageMeta
+bower1Data :: BowerJson
 bower1Data =
-  PackageMeta { name: "purescript-search-trie"
-              , license: Just [ "BSD-3-Clause" ]
-              , repository: Just  { type: "git"
-                                  , url: "git://github.com/klntsky/purescript-search-trie.git"
-                                  }
-              , description: Just "some description"
-              , main: Just [ "js/motion.js"
-                           , "sass/motion.scss"
-                           ]
-              , moduleType: Just [ AMD ]
-              , ignore:
-                Just [ "**/.*"
-                     , "node_modules"
-                     , "bower_components"
-                     , "output"
-                     ]
-              , keywords: Just [ "k1", "k2" ]
-              , authors: Just [ Author { name: Just "name1"
-                                       , email: Nothing
-                                       , homepage: Nothing
-                                       }
-                              , Author { name: Just "name2"
-                                       , email: Just "email"
-                                       , homepage: Just "homepage"
-                                       }
-                              ]
-              , homepage: Just "homepage"
-              , dependencies: Dependencies [
-                { packageName: "purescript-prelude", versionRange: "^4.0.0" },
-                { packageName: "purescript-arrays", versionRange: "^5.0.0" },
-                { packageName: "purescript-ordered-collections", versionRange: "^1.0.0" },
-                { packageName: "purescript-lists", versionRange: "^5.2.0" },
-                { packageName: "purescript-foldable-traversable", versionRange: "^4.0.0" },
-                { packageName: "purescript-bifunctors", versionRange: "^4.0.0" }
-                ]
-              , devDependencies: Dependencies [
-                { packageName: "purescript-assert", versionRange: "^4.1.0" },
-                { packageName: "purescript-psci-support", versionRange: "^4.0.0" },
-                { packageName: "purescript-strings", versionRange: "^4.0.0" },
-                { packageName: "purescript-effect", versionRange: "^2.0.0" }
-                ]
-              , resolutions: Just $ Resolutions
-                [ { packageName: "angular"
-                  , version: "1.3.0-beta.16"
-                  }
-                ]
-              , private: Nothing
-              }
+  BowerJson { name: "purescript-search-trie"
+            , license: Just [ "BSD-3-Clause" ]
+            , repository: Just  { type: "git"
+                                , url: "git://github.com/klntsky/purescript-search-trie.git"
+                                }
+            , description: Just "some description"
+            , main: Just [ "js/motion.js"
+                         , "sass/motion.scss"
+                         ]
+            , moduleType: Just [ AMD ]
+            , ignore:
+              Just [ "**/.*"
+                   , "node_modules"
+                   , "bower_components"
+                   , "output"
+                   ]
+            , keywords: Just [ "k1", "k2" ]
+            , authors: Just [ Author { name: Just "name1"
+                                     , email: Nothing
+                                     , homepage: Nothing
+                                     }
+                            , Author { name: Just "name2"
+                                     , email: Just "email"
+                                     , homepage: Just "homepage"
+                                     }
+                            ]
+            , homepage: Just "homepage"
+            , dependencies: Dependencies [
+              { packageName: "purescript-prelude", versionRange: "^4.0.0" },
+              { packageName: "purescript-arrays", versionRange: "^5.0.0" },
+              { packageName: "purescript-ordered-collections", versionRange: "^1.0.0" },
+              { packageName: "purescript-lists", versionRange: "^5.2.0" },
+              { packageName: "purescript-foldable-traversable", versionRange: "^4.0.0" },
+              { packageName: "purescript-bifunctors", versionRange: "^4.0.0" }
+              ]
+            , devDependencies: Dependencies [
+              { packageName: "purescript-assert", versionRange: "^4.1.0" },
+              { packageName: "purescript-psci-support", versionRange: "^4.0.0" },
+              { packageName: "purescript-strings", versionRange: "^4.0.0" },
+              { packageName: "purescript-effect", versionRange: "^2.0.0" }
+              ]
+            , resolutions: Just $ Resolutions
+              [ { packageName: "angular"
+                , version: "1.3.0-beta.16"
+                }
+              ]
+            , private: Nothing
+            }
+
+packageMeta1 :: PackageMeta
+packageMeta1 = PackageMeta { name: "..."
+                           , description: Nothing
+                           , main: []
+                           , moduleType: []
+                           , license: []
+                           , ignore: []
+                           , keywords: []
+                           , resolutions: mempty
+                           , private: false
+                           , dependencies: mempty
+                           , devDependencies: mempty
+                           , repository: Nothing
+                           , authors: []
+                           , homepage: Nothing
+                           }
+
+bowerJson2 :: BowerJson
+bowerJson2 =
+  BowerJson { name: "..."
+            , license: Nothing
+            , repository: Nothing
+            , description: Nothing
+            , main: Nothing
+            , moduleType: Nothing
+            , ignore: Nothing
+            , keywords: Nothing
+            , authors: Nothing
+            , homepage: Nothing
+            , dependencies: mempty
+            , devDependencies: mempty
+            , resolutions: Nothing
+            , private: Nothing
+            }
 
 main :: Effect Unit
 main = runTest do
   suite "decoding" do
+
+    suite "Author" do
+      test "decodeJson #1" do
+        let actual = parseDecode
+                     """
+                     "author"
+                     """
+            expected = Right $ Author { name: Just "author", email: Nothing, homepage: Nothing }
+        Assert.equal expected actual
+
+      test "decodeJson #2" do
+        let actual = parseDecode
+                     """
+                     { "name": "author", "homepage": "..." }
+                     """
+            expected = Right $ Author { name: Just "author", email: Nothing, homepage: Just "..." }
+        Assert.equal expected actual
 
     suite "Dependencies" do
 
@@ -130,7 +182,7 @@ main = runTest do
             expected = Right deps
         Assert.equal expected actual
 
-    suite "PackageMeta" do
+    suite "BowerJson" do
 
       test "decodeJson" do
         let actual = parseDecode bower1
@@ -142,6 +194,11 @@ main = runTest do
             expected = Right bower1
         Assert.equal expected actual
 
+    suite "PackageMeta" do
+      test "decodeJson" do
+        let actual = decodeJson $ encodeJson packageMeta1
+            expected = Right bowerJson2
+        Assert.equal expected actual
 
 parseDecode :: forall a. DecodeJson a => String -> Either String a
 parseDecode = decodeJson <=< jsonParser
